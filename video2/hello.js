766acd144d04b825a72860984f9c07bd38445183
ThanhNguyen <thanhnguyencoder@gmail.com>
2023-12-19 21:27:49 +0700

    First commit

---
+++ video2/hello.js
@@ -0,0 +1,7 @@
+console.log('Hello World From File JavaScript')
+console.log('Hello World With Thanh Nguyen')
+console.log(new Date().getDate())
+console.log(new Date().getFullYear())
+console.log(new Date().getUTCFullYear())
+console.log(new Date().getTimezoneOffset())
+
---
+++ video3/video3.js
@@ -0,0 +1,11 @@
+let x = 2.5;
+let y = 3;
+let z = 2;
+
+// console.log("Hello world form JS''", x == z)
+console.log('Hello world form JS""', y == z)
+console.log(`Hello '""""   '''""" world form JS""'`, typeof(x))
+
+let v=1, b=5, o=8;
+console.log('test string', 'b= ', b, 'v= ', v, 'o= ', o);
+
