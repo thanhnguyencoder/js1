507f6b29e1b3bf88c04c6d125b42fd1a92386aaa
ThanhNguyen <thanhnguyencoder@gmail.com>
2023-12-25 15:23:06 +0700

    Video 7

--- video4/video4.js
+++ video4/video4.js
@@ -1,2 +1,10 @@
 let a = 'Tôi là 1 string - chuỗi';
 console.log(a)
+console.log(a.length)
+console.log(4 + 5 + 'a')
+console.log('a'+ 5 + 4)
+console.log(a + 5 + 4);
+let Uppertext = a.toUpperCase();
+let Lowertext = a.toLowerCase();
+console.log(Uppertext);
+console.log(Lowertext);
