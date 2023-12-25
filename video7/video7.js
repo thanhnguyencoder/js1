bf41db78abcba7b80d057b263db3f5bc204692a9
ThanhNguyen <thanhnguyencoder@gmail.com>
2023-12-25 21:35:14 +0700

    Video 8

---
+++ video7/video7.js
@@ -0,0 +1,16 @@
+console.log('Hello world form HTML')
+
+const a = 5;
+const b = 10;
+console.log("Fifteen is " + (a + b) + " and\nnot " + (2 * a + b) + ".");
+console.log("Fifteen is " + a + b + " and\nnot " + (2 * a + b) + ".");
+
+// "Fifteen is 15 and
+// not 20."
+
+console.log(`Fifteen is ${a + b} and
+not ${2 * a + b}.`);
+// "Fifteen is 15 and
+// not 20."
+
+console.log(`a + b bằng ${a+b} có đúng không anh em?`)
