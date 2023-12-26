987dad9c30ec06f172a14ddc90d0d69d18cebb14
ThanhNguyen <thanhnguyencoder@gmail.com>
2023-12-26 15:22:29 +0700

    Video 8

---
+++ video8/video8.js
@@ -0,0 +1,11 @@
+let obj = {
+    name: 'Thanh',
+    age : 35,
+    address : `185 Tram Lac, My Hanh Bac, Duc Hoa, Long An`,
+    gender : `Male `
+};
+let b = 'name';
+// obj[b] = 'Uyen';
+obj.name = 'Hung';
+console.log (`Xuất giá trị name trong Object là ${obj['name']}`);
+console.log (`Giá trị của b là ${b}`);
