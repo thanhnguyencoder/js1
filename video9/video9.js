8798bd800c2a8b6949a082c43313dfd042335159
ThanhNguyen <thanhnguyencoder@gmail.com>
2024-01-03 22:59:18 +0700

    Video 9

---
+++ video9/video9.js
@@ -0,0 +1,18 @@
+console.log('hello world from HTML')
+let a = {
+    name: 'Thanh',
+    age: 35
+};//key:value
+
+console.log('Check data object: ', a)
+console.log('My name\'s: ', a.name)
+
+let b = ['MU', 'Chelsea', 'Liver'];
+console.log ('Typof b: ',typeof b, b)
+console.log ('First item: ', b[0], ' Second item: ', b[1])
+
+let family = [
+    {name: 'Ly', age: 36},
+    {name: 'Chip', age: 35},
+]
+console.log('my family: ', family)
