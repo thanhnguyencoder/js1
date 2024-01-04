72035d5c55058abd7930ba86bd846b35dbded653
ThanhNguyen <thanhnguyencoder@gmail.com>
2024-01-04 16:34:57 +0700

    Video 11

---
+++ video11/video11.js
@@ -0,0 +1,12 @@
+console.log('Hello world from HTML')
+let i = 2;
+// let arr = ['Thanh', 'Ly', 'Chip', 'Hat De']
+// for(let i=0; i<arr.length; i++ ){
+//     console.log(`Name: ${arr[i]}`)
+// }
+// console.log('value i: ',i)
+
+// let b = i++;
+let c = i++;
+// console.log('i: ', b)
+console.log('c: ', c)
