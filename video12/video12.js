c1971f3abdbf4fdc2568df67596c5e82e4c6ea5a
ThanhNguyen <thanhnguyencoder@gmail.com>
2024-01-04 17:01:09 +0700

    Video 12

---
+++ video12/video12.js
@@ -0,0 +1,17 @@
+console.log('Hello world from HTML')
+let arrTop4 = ['ManCity','Liverpool','Mu','Chelsea'];
+// for(let i=0; i<arrTop4.length; i++){
+//     console.log(`Top: ${i+1} ${arrTop4[i]}`)
+// }
+
+let i=0;
+// while(i < arrTop4.length){
+//     console.log(`Top: ${i+1} ${arrTop4[i]}`);
+//     i++;
+// }
+
+do{
+    console.log(`Top: ${i+1} ${arrTop4[i]}`);
+    i++;
+}
+while(i>arrTop4.length)
