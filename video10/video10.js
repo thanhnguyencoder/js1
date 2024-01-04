51dfdc28ac655e6e157bf4854a2a3da452e705b3
ThanhNguyen <thanhnguyencoder@gmail.com>
2024-01-04 15:51:01 +0700

    Video 10

---
+++ video10/video10.js
@@ -0,0 +1,7 @@
+console.log(`Hello world from HTML`)
+
+let a = 5; // x <- y
+let b = '5';
+
+console.log(`compare a == b: a = ${a}, b = ${b}, result: ${a==b}`)
+console.log(`compare a === b: a = ${a}, b = ${b}, result: ${a===b}`)
