95326dd52166969b47a17553edba68e846b25343
ThanhNguyen <thanhnguyencoder@gmail.com>
2024-01-09 17:01:17 +0700

    Function

--- video15/video15.js
+++ video15/video15.js
@@ -1,1 +1,16 @@
-console.log(`Hello World from HTML`)
+console.log(`Hello World from HTML`);
+
+function addNumbers(a, b) {
+    const sum = a + b;
+    return sum;
+  }
+  
+function Loi(d, e) {
+    return d - e;
+}
+let xLoi = Loi(9,5)
+console.log (`d - e = `, xLoi);
+
+  // Calling the function
+  const result = addNumbers(5, 7);
+  console.log(result); // Output: 12
