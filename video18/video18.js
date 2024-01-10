9f867dfba2d7db2f82f77cfe74e15d8eb2d3a483
ThanhNguyen <thanhnguyencoder@gmail.com>
2024-01-10 15:30:13 +0700

    callback - setTimeout - SetInterval

--- video18/video18.js
+++ video18/video18.js
@@ -1,1 +1,22 @@
 console.log(`Hello world from HTML`)
+
+let sum = (a,b,callback) => {
+    let tong = a + b;
+    // setTimeout(() => {
+    //     callback(tong);
+    // }, 5000)
+    let i = 0;
+    let timer = setInterval(() => {
+        callback(tong);
+        i++;
+        if(i === 5) {
+            clearInterval(timer);
+        }
+    }, 1000)
+}
+
+let printsum = (message) => {
+    console.log(`Check sum a + b = `, message)
+}
+
+sum(6,9,printsum);
