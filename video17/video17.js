f95980344eeac5c95e14ebd7064d32295c7c6099
ThanhNguyen <thanhnguyencoder@gmail.com>
2024-01-10 10:45:38 +0700

    Arrow function

---
+++ video17/video17.js
@@ -0,0 +1,11 @@
+console.log(`Hello world from HTML`)
+
+// function sum(a, b){
+//     return a+b;
+// }
+
+// arrow function
+let sum = (a,b) => {
+    return a + b;
+}
+console.log(`Sum a and b:`,sum(5,2));
