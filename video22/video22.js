43eb3caa67358c68ba71053c8c102373d6977bb6
ThanhNguyen <thanhnguyencoder@gmail.com>
2024-01-11 11:00:42 +0700

    Change to Map

--- video22/video22.js
+++ video22/video22.js
@@ -1,1 +1,14 @@
 console.log(`Hello world from HTML`)
+
+let arr = [1,2,3,4,5,6,7,8,9,10]
+// for (let i = 0; i < arr.length; i++) {
+//     arr[i] = arr[i] * arr[i];
+// }
+
+// console.log(`check value arr: `, arr)
+
+let mapArr = arr.map((item, index) => {
+    return item * item;
+})
+
+console.log(mapArr)
