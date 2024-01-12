b7ca01c587f1807f4519f1e49b260d24c238675c
ThanhNguyen <thanhnguyencoder@gmail.com>
2024-01-12 10:36:55 +0700

    Map v2

--- video22/video22.js
+++ video22/video22.js
@@ -1,4 +1,4 @@
-console.log(`Hello world from HTML`)
+console.log(`Hello world from HTML - Map`)
 
 let arr = [1,2,3,4,5,6,7,8,9,10]
 // for (let i = 0; i < arr.length; i++) {
@@ -8,7 +8,10 @@ let arr = [1,2,3,4,5,6,7,8,9,10]
 // console.log(`check value arr: `, arr)
 
 let mapArr = arr.map((item, index) => {
-    return item * item;
+    return (
+        `<td>${item} - ${index}</td>`
+    );
 })
 
-console.log(mapArr)
+console.log('Arr',arr)
+console.log('Map Arr',mapArr)
